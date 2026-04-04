c28c7e5a43f488119e815ba4d097b5366134da3c
dieAssen <49062364+dieAssen@users.noreply.github.com>
2026-04-04 22:02:26 +0200

    fix: revert to /serverless import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   site: 'https://asn-pfeil-phoenix.vercel.app',
